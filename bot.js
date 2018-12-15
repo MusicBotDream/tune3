7f7c96171d04014bb447979c4e9301bcc867ac3e
SystemsBotDream <45539387+SystemsBotDream@users.noreply.github.com>
2018-12-15 10:29:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -343,7 +343,7 @@ client.on('message', message => {
     }
 });
 
-const developers = ["472413769700474901","id"]
+const developers = ["472413769700474901","486149211595866112"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
